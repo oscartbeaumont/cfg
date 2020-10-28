4fae771fe18833d993b0448823134791a66b788b
Oscar Beaumont <oscar@otbeaumont.me>
2020-10-28 19:30:46 +0800

    feat: Added Ubuntu on Linux Setup Script

---
+++ deploy-ubuntu-on-win.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Enable "Windows Subsystem for Linux" Feature
+# Install "Ubuntu 20.04 LTS"
+
+touch /home/oscar/.hushlogin
+sudo apt-get update && sudo apt-get -y upgrade
+
+# Go Lang
+wget -O go.tar.gz https://dl.google.com/go/go1.15.3.linux-amd64.tar.gz
+tar -xvf go.tar.gz
+sudo mv go /usr/local
+rm -rf go.tar.gz
+
+# ~/.bashrc & /root/.bashrc
+    export GOROOT=/usr/local/go
+    export GOPATH=$HOME/go
+    export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+
+mkdir $GOPATH
+
+# SQLC
+wget -O sqlc.tgz https://bin.equinox.io/c/jF3LhnJK5xn/sqlc-stable-linux-amd64.tgz
+tar -xvf go.tar.gz
+sudo mv sqlc /usr/local/bin
+
+# Nodejs
+curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# NPM Usefull Packages
+npm i -g npm-check-updates serve
